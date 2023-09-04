60704d73c2bc88bca3fdb55b2717b14ea6379a8b
mdsiamsheikh <mdsiamsheikh6640@gmail.com>
2023-09-04 19:59:34 +0600

    updath this navber center

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -3,9 +3,9 @@
 <template>
   <div>
     <nav class="bg-cyan-900">
-      <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div class="max-w-7xl mx-auto pl-2 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
-          <div class="mt-4 sm:ml-6 sm:flex sm:items-center">
+          <div class="flex items-center">
             <NuxtLink
               to="/"
               class="px-3 py-2 rounded-md text-sm font-medium text-white hover:bg-cyan-700 focus:outline-none focus:bg-cyan-700"
