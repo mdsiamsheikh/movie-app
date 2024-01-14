b71512f788cefdfa18f0f4fed545e56e4621c664
mdsiamsheikh <mdsiamsheikh6640@gmail.com>
2024-01-14 17:47:49 +0600

    add a color change

--- components/Hero.vue
+++ components/Hero.vue
@@ -5,7 +5,7 @@
   >
     <div class="container sm:mx-auto sm:p-32 p-8">
       <h1 class="text-6xl font-bold text-rose-600 mb-4">Movilix</h1>
-      <h3 class="text-lg font-bold text-black">
+      <h3 class="text-lg font-bold text-white">
         Unlimited movies, TV shows and more.
       </h3>
       <div class="mt-6">
