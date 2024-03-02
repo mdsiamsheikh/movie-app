211043100540e324c325678aa9d1d44196da2db7
siam <mdsiamsheikh6640@gmail.com>
2024-03-02 19:36:40 +0600

    create a new navbar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,36 +1,41 @@
 <script setup></script>
 
 <template>
-  <div>
-    <nav class="bg-cyan-900">
-      <div class="max-w-7xl px-1 mx-auto pl-2 sm:px-6 lg:px-8">
-        <div class="flex justify-between h-16">
-          <div class="flex items-center">
-            <NuxtLink
-              to="/"
-              class="px-3 py-2 rounded-md text-sm font-medium text-white hover:bg-cyan-700 focus:outline-none focus:bg-cyan-700"
-              >Home</NuxtLink
-            >
-            <NuxtLink
-              to="/login"
-              class="px-3 py-2 rounded-md text-sm font-medium text-white hover:bg-cyan-700 focus:outline-none focus:bg-cyan-700"
-              >Login</NuxtLink
-            >
-            <NuxtLink
-              to="/registration"
-              class="px-3 py-2 rounded-md text-sm font-medium text-white hover:bg-cyan-700 focus:outline-none focus:bg-cyan-700"
-              >Registration</NuxtLink
-            >
-            <NuxtLink
-              to="/contact"
-              class="px-3 py-2 rounded-md text-sm font-medium text-white hover:bg-cyan-700 focus:outline-none focus:bg-cyan-700"
-              >Contact</NuxtLink
-            >
-          </div>
-        </div>
+  <nav class="bg-white border-gray-200 dark:bg-gray-900">
+    <div
+      class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4"
+    >
+      <NuxtLink to="/" class="flex items-center space-x-3 rtl:space-x-reverse">
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrZ9AKRWSxh4v4fPLk9hgKnYls7wkHMrVDGQ&usqp=CAU"
+          class="h-8 w-8"
+          alt=""
+        />
+        <span
+          class="rounded-md font-medium text-2xl text-[#011A24] hover:text-cyan-700"
+          >Home</span
+        >
+      </NuxtLink>
+
+      <div class="flex items-center space-x-6 rtl:space-x-reverse">
+        <NuxtLink
+          to="/contact"
+          class="px-2 py-1 rounded-md font-medium text-xl text-[#011A24] hover:text-cyan-700"
+          >Contact</NuxtLink
+        >
+        <NuxtLink
+          to="/registration"
+          class="px-2 py-1 rounded-md font-medium text-xl text-[#011A24] hover:text-cyan-700"
+          >Registration</NuxtLink
+        >
+        <NuxtLink
+          to="/login"
+          class="px-2 py-1 rounded-md font-medium text-xl text-[#011A24] hover:text-cyan-700"
+          >Login</NuxtLink
+        >
       </div>
-    </nav>
-  </div>
+    </div>
+  </nav>
 </template>
 
 <style lang="css" scoped></style>
